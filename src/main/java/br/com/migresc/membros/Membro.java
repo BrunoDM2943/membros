157ecd44cad893da5e6fef2a05352f6cca8a65b5
BrunoDM2943 <bdm2943@gmail.com>
2017-01-16 21:28:56 -0200

    Initial commit

---
+++ src/main/java/br/com/migresc/membros/Membro.java
@@ -0,0 +1,29 @@
+package br.com.migresc.membros;
+
+/**
+ * Classe Membro
+ * @author bruno
+ *
+ */
+public final class Membro {
+
+    /**
+     * Nome do membro
+     */
+    private String nome;
+
+    /**
+     * @return the nome
+     */
+    public final String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public final void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
---
+++ src/test/java/test/br/com/migresc/membros/PrimaryTest.java
@@ -0,0 +1,15 @@
+package test.br.com.migresc.membros;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimaryTest {
+
+    @Test
+    public void test(){
+        System.out.println("Teste");
+        assertTrue(5==5);
+    }
+
+}
