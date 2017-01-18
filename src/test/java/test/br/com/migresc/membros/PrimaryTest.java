e20896ada0436bdf6cbcff40d82179ac744bb9ba
BrunoDM2943 <bdm2943@gmail.com>
2017-01-17 23:00:32 -0200

    Just some test

--- src/test/java/test/br/com/migresc/membros/PrimaryTest.java
+++ src/test/java/test/br/com/migresc/membros/PrimaryTest.java
@@ -2,14 +2,16 @@ package test.br.com.migresc.membros;
 
 import static org.junit.Assert.*;
 
+import br.com.migresc.membros.Membro;
 import org.junit.Test;
 
 public class PrimaryTest {
 
     @Test
     public void test(){
-        System.out.println("Teste");
-        assertTrue(5==5);
+        Membro membro = new Membro();
+        membro.setNome("Bruno");
+        assertEquals("Bruno", membro.getNome());
     }
 
 }
